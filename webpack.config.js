21e74c8ac4b98268160f6f8e0a748b546c6af40a
Christopher Roussel <christopher@impleri.net>
2016-05-04 21:49:48 -0500

    Adjusting webpack config to switch handling depending on environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,43 @@
 "use strict";
 
 var path = require("path"),
-    webpack = require("webpack");
+    webpack = require("webpack"),
+    production = {
+        devtool: "#hidden-source-map",
+        entry: ["eventsource-polyfill"],
+        plugins: [
+          new webpack.optimize.OccurenceOrderPlugin(),
+          new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+              warnings: false
+            }
+          })
+        ]
+    },
+    development = {
+      devtool: "#cheap-inline-source-map",
+      entry: production.entry.concat(["webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000",]),
+      plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+      ]
+    },
+    config = (process.env.NODE_ENV === "production") ? production : development;
 
 module.exports = {
-  devtool: "#inline-source-map",
-  entry: [
-    "'eventsource-polyfill",
-    "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000",
-    "./client/index.js"
-  ],
-
+  devtool: config.devtool,
+  entry: config.entry.concat(["./client/index.js"]),
   output: {
     path: path.join(__dirname, "build"),
     filename: "bundle.js",
     publicPath: "/assets/static/"
   },
-
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-
+  plugins: config.plugins,
   resolve: {
     alias: {},
     extensions: ["", ".js", ".jsx"]
   },
-
   module: {
     loaders: [
       {
